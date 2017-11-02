a79bd8e32ec829b381b8809148f568e0c0f41c13
gtan049 <33273771+gtan049@users.noreply.github.com>
2017-11-03 01:32:09 +0800

    Update Account.java

--- MovieApp/src/Account.java
+++ MovieApp/src/Account.java
@@ -1,4 +1,15 @@
-
 public class Account {
-	public String userID;
+	private String userID;
+	
+	public Account(String userID) {
+		this.userID = userID;
+	}
+	
+	public String getUserID() {
+		return userID;
+	}
+	
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
 }
