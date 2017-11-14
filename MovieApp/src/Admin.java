55fa66d293fe8ad58dce318e0e8f5a10c210bfcf
rhowardliu <rhowardliu@gmail.com>
2017-11-15 03:55:00 +0800

    minor change admin

--- MovieApp/src/Admin.java
+++ MovieApp/src/Admin.java
@@ -376,7 +376,7 @@ public class Admin extends Account {
 			
 		System.out.println("Select cinema");
 		for (int j = 0; j < tempcinemaarray.length ; j++)
-			System.out.println("(" + j+1 + ")" + tempcinemaarray[j].getCinemaCode());
+			System.out.println("(" + (j+1) + ")" + tempcinemaarray[j].getCinemaCode());
 		int cinemachoice = sc.nextInt();
 		CinemaTypeEnum cinematype = CinemaTypeEnum._standard;
 		String cinemacode = tempcinemaarray[cinemachoice - 1].getCinemaCode();
